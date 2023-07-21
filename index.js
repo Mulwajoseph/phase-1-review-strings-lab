0590cd551469f3b165c91dc0d61c9f68458ef51f
mulwajoseph <mulwajose.jm@gmail.com>
2023-07-21 12:34:31 +0300

    string

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+
+
+const currentUser ="Mulwa Joseph";
+const welcomeMessage ="Welcome to Flatbook, "+ currentUser +"!";
+const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}, ${currentUser} !`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)} !`
